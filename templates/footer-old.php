e63838bd198029ff13cb6cebe19d561c592e2a7b
John Rey Sto Domingo <stodomingojohnrey5@gmail.com>
2023-04-05 04:28:35 +0800

    Templates

--- templates/footer-old.php
+++ templates/footer-old.php
@@ -1,3 +1,66 @@
+<footer id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="footer-single-box">
+                        <img src="assets/logo/logo_100x100.png" alt="">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam repellendus sunt praesentium aspernatur iure molestias.</p>
+                    <h3>We Accept</h3>
+                    <div class="card-area">
+                            <i class="fa-brands fa-cc-visa"></i>
+                            <i class="fa-solid fa-credit-card"></i>
+                            <i class="fa-brands fa-cc-mastercard"></i>
+                            <i class="fa-brands fa-cc-paypal"></i>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="footer-single-box">
+                        <h2>Hosting</h2>
+                    <ul>
+                        <li><a href="#">Web Hosting</a></li>
+                        <li><a href="#">Cloud Hosting</a></li>
+                        <li><a href="#">CMS Hosting</a></li>
+                        <li><a href="#">WordPress Hosting</a></li>
+                        <li><a href="#">Email Hosting</a></li>
+                        <li><a href="#">VPS Hosting</a></li>
+                    </ul>
+                    </div>                    
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="footer-single-box">
+                        <h2>Domain</h2>
+                    <ul>
+                        <li><a href="#">Web Domain</a></li>
+                        <li><a href="#">Cloud Domain</a></li>
+                        <li><a href="#">CMS Domain</a></li>
+                        <li><a href="#">WordPress Domain</a></li>
+                        <li><a href="#">Email Domain</a></li>
+                        <li><a href="#">VPS Domain</a></li>
+                    </ul>
+                    </div>                    
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="footer-single-box">
+                        <h2>Newsletter</h2>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur doloremque earum similique fugiat nobis. Facere?</p>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Enter your Email ..." aria-describedby="basic-addon2">
+                            <span class="input-group-text" id="basic-addon2"><i class="fa fa-long-arrow-right"></i></span>
+                        </div>
+                        <h2>Follow us on</h2>
+                        <p class="footer-socials">
+                            <i class="fa-brands fa-facebook-f"></i>
+                            <i class="fa-brands fa-dribbble"></i>
+                            <i class="fa-brands fa-pinterest"></i>
+                            <i class="fa-brands fa-twitter"></i>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
 <!-- Popper and Bootstrap 5.3 -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
--- templates/header-old.php
+++ templates/header-old.php
@@ -14,6 +14,8 @@
     <!-- Font Awesome JS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="assets/css/style.css">
-    <title>Destiny Driving School</title>
-</head>
-<body> 
+
+
+<?php
+    include("templates/modals.php");
+?>
